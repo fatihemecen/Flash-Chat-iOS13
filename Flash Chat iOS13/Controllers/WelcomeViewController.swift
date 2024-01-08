2d50b5dcbe2ebc8cd28a07e26bf786166b1f0962
Fatih Emecen <fatih.emecen@ozu.edu.tr>
2024-01-08 19:36:34 +0300

    FlashChat

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = Constants.appName
+        
+        
+//        titleLabel.text = ""
+//        var charIndex = 0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
     }
     
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+
+target 'Flash Chat iOS13' do
+  #use_frameworks!
+
+  # Pods for Flash Chat iOS13
+  
+end
